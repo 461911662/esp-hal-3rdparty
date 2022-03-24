461911662/esp-hal-3rdparty

commit 2cf19f0dc9acff11750ecbeb1c5a8f9f6e196a31

spi: allow using esp_flash and spi_master driver on the same bus

components/driver/include/driver/spi_common.h
@@ -102,7 +102,7 @@ typedef struct {
  *
  * @warning For now, only supports HSPI and VSPI.
  *
- * @param host SPI peripheral that controls this bus
+ * @param host_id SPI peripheral that controls this bus
  * @param bus_config Pointer to a spi_bus_config_t struct specifying how the host should be initialized
  * @param dma_chan Either channel 1 or 2, or 0 in the case when no DMA is required. Selecting a DMA channel
  *                 for a SPI bus allows transfers on the bus to have sizes only limited by the amount of
@@ -123,20 +123,20 @@ typedef struct {
  *         - ESP_ERR_NO_MEM        if out of memory
  *         - ESP_OK                on success
  */
-esp_err_t spi_bus_initialize(spi_host_device_t host, const spi_bus_config_t *bus_config, int dma_chan);
+esp_err_t spi_bus_initialize(spi_host_device_t host_id, const spi_bus_config_t *bus_config, int dma_chan);
 
 /**
  * @brief Free a SPI bus
  *
  * @warning In order for this to succeed, all devices have to be removed first.
  *
- * @param host SPI peripheral to free
+ * @param host_id SPI peripheral to free
  * @return
  *         - ESP_ERR_INVALID_ARG   if parameter is invalid
  *         - ESP_ERR_INVALID_STATE if not all devices on the bus are freed
  *         - ESP_OK                on success
  */
-esp_err_t spi_bus_free(spi_host_device_t host);
+esp_err_t spi_bus_free(spi_host_device_t host_id);
 
 #ifdef __cplusplus
 }

components/driver/include/driver/spi_master.h
@@ -171,7 +171,7 @@ typedef struct spi_device_t* spi_device_handle_t;  ///< Handle for a device on a
  * @note While in general, speeds up to 80MHz on the dedicated SPI pins and 40MHz on GPIO-matrix-routed pins are
  *       supported, full-duplex transfers routed over the GPIO matrix only support speeds up to 26MHz.
  *
- * @param host SPI peripheral to allocate device on
+ * @param host_id SPI peripheral to allocate device on
  * @param dev_config SPI interface protocol config for the device
  * @param handle Pointer to variable to hold the device handle
  * @return
@@ -180,7 +180,7 @@ typedef struct spi_device_t* spi_device_handle_t;  ///< Handle for a device on a
  *         - ESP_ERR_NO_MEM        if out of memory
  *         - ESP_OK                on success
  */
-esp_err_t spi_bus_add_device(spi_host_device_t host, const spi_device_interface_config_t *dev_config, spi_device_handle_t *handle);
+esp_err_t spi_bus_add_device(spi_host_device_t host_id, const spi_device_interface_config_t *dev_config, spi_device_handle_t *handle);
 
 
 /**

components/soc/include/hal/spi_types.h
@@ -21,12 +21,10 @@
  * @brief Enum with the three SPI peripherals that are software-accessible in it
  */
 typedef enum {
+// SPI_HOST (SPI1_HOST) is not supported by the SPI Master and SPI Slave driver on ESP32-S2
     SPI1_HOST=0,    ///< SPI1
     SPI2_HOST=1,    ///< SPI2
     SPI3_HOST=2,    ///< SPI3
-#if SOC_SPI_PERIPH_NUM > 3
-    SPI4_HOST=3,    ///< SPI4
-#endif
 } spi_host_device_t;
 
 //alias for different chips
@@ -35,6 +33,7 @@ typedef enum {
 #define HSPI_HOST   SPI2_HOST
 #define VSPI_HOST   SPI3_HOST
 #elif CONFIG_IDF_TARGET_ESP32S2
+// SPI_HOST (SPI1_HOST) is not supported by the SPI Master and SPI Slave driver on ESP32-S2
 #define SPI_HOST    SPI1_HOST
 #define FSPI_HOST   SPI2_HOST
 #define HSPI_HOST   SPI3_HOST

components/soc/soc/esp32/include/soc/spi_caps.h
@@ -16,8 +16,7 @@
 
 #define SOC_SPI_PERIPH_NUM      3
 #define SOC_SPI_DMA_CHAN_NUM    2
-
-#define SPI_PERIPH_NUM          3
+#define SOC_SPI_PERIPH_CS_NUM(i)    3
 
 #define SPI_FUNC_NUM            1
 #define SPI_IOMUX_PIN_NUM_MISO  7

components/soc/soc/esp32s2/include/soc/spi_caps.h
@@ -14,8 +14,9 @@
 
 #pragma once
 
-#define SOC_SPI_PERIPH_NUM      4
+#define SOC_SPI_PERIPH_NUM      3
 #define SOC_SPI_DMA_CHAN_NUM    3
+#define SOC_SPI_PERIPH_CS_NUM(i)    3
 
 #define SPI_FUNC_NUM            0
 #define SPI_IOMUX_PIN_NUM_HD    27
@@ -35,7 +36,7 @@
 #define FSPI_IOMUX_PIN_NUM_WP   14
 //TODO: add the next slot
 
-//HSPI and VSPI have no iomux pins
+//HSPI has no iomux pins
 
 #define SOC_SPI_MAXIMUM_BUFFER_SIZE     72
 

components/spi_flash/esp_flash_spi_init.c
@@ -132,7 +132,10 @@ esp_err_t spi_bus_add_flash_device(esp_flash_t **out_chip, const esp_flash_spi_d
         .read_mode = config->io_mode,
         .host = host,
     };
-    esp_err_t err = esp_flash_init_os_functions(chip, config->host_id);
+
+    int dev_id;
+    esp_err_t err = esp_flash_init_os_functions(chip, config->host_id, &dev_id);
+    assert(dev_id < SOC_SPI_PERIPH_CS_NUM(config->host_id) && dev_id >= 0);
     if (err != ESP_OK) {
         ret = err;
         goto fail;
@@ -141,7 +144,7 @@ esp_err_t spi_bus_add_flash_device(esp_flash_t **out_chip, const esp_flash_spi_d
     bool use_iomux = spicommon_bus_using_iomux(config->host_id);
     memspi_host_config_t host_cfg = {
         .host_id = config->host_id,
-        .cs_num = config->cs_id,
+        .cs_num = dev_id,
         .iomux = use_iomux,
         .input_delay_ns = config->input_delay_ns,
         .speed = config->speed,
@@ -165,6 +168,7 @@ esp_err_t spi_bus_remove_flash_device(esp_flash_t *chip)
     if (chip==NULL) {
         return ESP_ERR_INVALID_ARG;
     }
+    esp_flash_deinit_os_functions(chip);
     if (chip->host) {
         free(chip->host->driver_data);
         free(chip->host);

components/spi_flash/include/esp_flash_internal.h
@@ -16,6 +16,7 @@
 #include "esp_err.h"
 #include <stdint.h>
 #include <stdbool.h>
+#include <driver/spi_common_internal.h>
 #include "sdkconfig.h"
 
 #include "esp_flash.h"
@@ -68,12 +69,21 @@ esp_err_t esp_flash_app_disable_protect(bool disable);
  *
  * @param chip The chip to init os functions.
  * @param host_id Which SPI host to use, 1 for SPI1, 2 for SPI2 (HSPI), 3 for SPI3 (VSPI)
+ * @param out_dev_id Output of occupied device slot
  *
  * @return
  *      - ESP_OK if success
  *      - ESP_ERR_INVALID_ARG if host_id is invalid
  */
-esp_err_t esp_flash_init_os_functions(esp_flash_t *chip, int host_id);
+esp_err_t esp_flash_init_os_functions(esp_flash_t *chip, int host_id, int *out_dev_id);
+
+/**
+ * @brief Deinitialize OS-level functions
+ *
+ * @param chip  The chip to deinit os functions
+ * @return always ESP_OK.
+ */
+esp_err_t esp_flash_deinit_os_functions(esp_flash_t* chip);
 
 /**
  *  Initialize OS-level functions for the main flash chip.

commit db23c55653533fe903e9c45537fa7f188c19b5c4

spi: fixed crash when calling spi_bus_free when not initialized
introduced in 2cf19f0dc9acff11750ecbeb1c5a8f9f6e196a31

Closes: [Github: espressif/esp-idf]/issues/8642

components/driver/include/driver/spi_common.h
@@ -159,7 +159,7 @@ esp_err_t spi_bus_initialize(spi_host_device_t host_id, const spi_bus_config_t *
  * @param host_id SPI peripheral to free
  * @return
  *         - ESP_ERR_INVALID_ARG   if parameter is invalid
- *         - ESP_ERR_INVALID_STATE if not all devices on the bus are freed
+ *         - ESP_ERR_INVALID_STATE if bus hasn't been initialized before, or not all devices on the bus are freed
  *         - ESP_OK                on success
  */
 esp_err_t spi_bus_free(spi_host_device_t host_id);